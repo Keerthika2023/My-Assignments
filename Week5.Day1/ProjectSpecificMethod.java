eb3fd6f6ab2ef6efbe2e066bcd23640d89876dea
Keerthika2023 <126364208+Keerthika2023@users.noreply.github.com>
2023-03-30 10:35:50 +1300

    Add files via upload

---
+++ Week5.Day1/ProjectSpecificMethod.java
@@ -0,0 +1,37 @@
+package week5.Day1;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ProjectSpecificMethod {
+
+	public ChromeDriver driver;
+	public ChromeOptions option;
+	@BeforeMethod
+	public void PreCondition() {
+		WebDriverManager.chromedriver().setup();
+		option=new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+		driver=new ChromeDriver(option);
+		driver.get("https://www.leafground.com/frame.xhtml");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+	}
+	@AfterMethod
+	public void PostCondition() {
+		driver.close();
+	}
+}
